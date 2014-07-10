20e0cd9a89ad0e4aa1c377a0ed00b98c822260f2
Ryan Tsao <ryan.j.tsao@gmail.com>
2014-07-10 01:54:39 -0700

    Added arrow key navigation

--- js/bundle.js
+++ js/bundle.js
@@ -15982,7 +15982,7 @@ return jQuery;
 },{}],3:[function(require,module,exports){
 var sudoku = require('./sudoku');
 
-sudoku.generateBoard();
+sudoku.init();
 },{"./sudoku":5}],4:[function(require,module,exports){
 var _ = require('lodash')
 
@@ -16034,20 +16034,61 @@ module.exports = (function($, _){
 
   }
 
-  return {
- 
-    generateBoard: function() {
-      var row, col,
-        rows = generateRootSolution();
+  function generateBoard() {
+    var row, col, rows = generateRootSolution();
 
-      for (row = 0; row < 9; row++) {
-        for (col = 0; col < 9; col++) {
-          var val = rows[row][col];
-          getCell(row, col).val(val);
-        }
+    for (row = 0; row < 9; row++) {
+      for (col = 0; col < 9; col++) {
+        var val = rows[row][col];
+        getCell(row, col).val(val);
+      }
+    }
+
+  }
+
+  function focusCell(row, col) {
+    getCell(row, col).focus().select();
+  }
+
+  function handleCellKeyDown(e) {
+    
+    // Arrow keys
+    if (37 <= e.keyCode && e.keyCode <= 40) {
+      e.preventDefault();
+
+      var col = parseInt($(this).attr('col'));
+      var row = parseInt($(this).attr('row'));
+
+      switch (e.keyCode) {
+        case 37:
+          col--;
+          break;
+        case 38:
+          row--;
+          break;
+        case 39:
+          col++;
+          break;
+        case 40:
+          row++;
+          break;
       }
 
+      focusCell((row+9) % 9, (col+9) % 9);
     }
+
+  }
+
+
+
+  return {
+    init: function() {
+      generateBoard();
+
+      $('.board').on('keydown', 'input', handleCellKeyDown);
+
+    }
+
  
   }
 })($, _);
